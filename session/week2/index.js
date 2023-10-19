fa5e0220545269be6f7e97a7f2c029c89121b4f2
Soyoung Park <soyoung008@naver.com>
2023-10-20 02:26:42 +0900

    2차 세미나 실습

---
+++ session/week2/index.js
@@ -0,0 +1,23 @@
+const mango=document.createElement("li");
+const mangoText = document.createTextNode("mango");
+
+mango.appendChild(mangoText);
+
+const fruitList = document.querySelector("ul");
+fruitList.appendChild(mango);
+
+const redFruit = document.querySelectorAll(".red");
+
+redFruit.forEach((fruit)=>{
+    fruit.remove();
+})
+
+const thirdFruit = document.querySelector("li:nth-child(3)");
+thirdFruit.classList.add("blue");
+
+const allList = document.querySelectorAll("li");
+const lengthButton = document.querySelector("button.count");
+function showLength(){
+    alert(`과일 개수는 ${allList.length}입니다.`);
+}
+lengthButton.addEventListener("click",showLength);
