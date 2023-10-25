25293c20543898b618a71d53259b4ad1ead3d305
Soyoung Park <soyoung008@naver.com>
2023-10-25 13:41:47 +0900

    feat : 말줄임표, 더보기 버튼 구현

--- assignment/week2/웨비의사진관/index.js
+++ assignment/week2/웨비의사진관/index.js
@@ -1,8 +1,26 @@
+
 const topBtn = document.getElementById("top_btn");
 const height = document.querySelector("body");
-
-document.addEventListener('scroll', ()=>{
+document.addEventListener('scroll', () => {
   let scrollPosition = window.scrollY;
   let scrollMax = height.scrollHeight - screen.height;
   topBtn.style.opacity = scrollPosition/scrollMax;
-})
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  createButton();
+});
+
+function createButton(){
+  const hover_content = document.querySelectorAll(".hover_content");
+  const hover_info = document.querySelector(".hover_info");
+
+  hover_content.forEach((item) =>{
+    if(item.innerHTML.length >= 40){
+      const more_button = document.createElement("button")
+      more_button.className="moreBtn";
+      more_button.textContent = "더보기";
+      item.parentNode.appendChild(more_button);
+    }
+  });
+}
