7070e837287c83e1937e8db112f37b74579910cc
Soyoung Park <soyoung008@naver.com>
2023-10-26 02:43:57 +0900

    feat : 카테고리 추가(localStorage 활용 전)

--- assignment/week2/가계부/category.js
+++ assignment/week2/가계부/category.js
@@ -1,17 +1,21 @@
 const CATEGORY_LIST = [
   {
+    id: 1,
     type: "spend",
     name: "식비"
   },
   {
+    id: 2,
     type: "spend",
     name: "쇼핑"
   },
   {
+    id: 3,
     type: "income",
     name: "용돈"
   },
   {
+    id: 4,
     type: "income",
     name: "월급"
   }
@@ -38,5 +42,29 @@ function createCategory(item) {
   }
   else {
     income_container.appendChild(li);
+  }
+}
+
+function addCategory(e) {
+  const income_container = document.querySelector(".income_category_list");
+  const spend_container = document.querySelector(".spend_category_list");
+  let category_type;
+  if (window.event.keyCode == 13) {
+    if (e.id == "income_category_input") {
+      category_type = "income";
+    }
+    else {
+      category_type = "spend";
+    }
+    const new_category = {
+      id: CATEGORY_LIST.length + 1,
+      type: category_type,
+      name: e.value
+    }
+    CATEGORY_LIST.push({ ...new_category });
+
+    income_container.innerHTML = "";
+    spend_container.innerHTML = "";
+    displayCategory(CATEGORY_LIST);
   }
 }
