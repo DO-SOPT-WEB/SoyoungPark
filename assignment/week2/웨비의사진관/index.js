4a50cc4920e1ca278a87a1a8a01e91e7217b56ea
Soyoung Park <soyoung008@naver.com>
2023-10-26 22:36:46 +0900

    feat : 더보기 버튼 3줄 이상일 때만 나오게 수정

--- assignment/week2/웨비의사진관/index.js
+++ assignment/week2/웨비의사진관/index.js
@@ -1,46 +1,43 @@
-
-const topBtn = document.getElementById("top_btn");
-const height = document.querySelector("body");
+const topBtn = document.getElementById('top_btn');
+const height = document.querySelector('body');
 document.addEventListener('scroll', () => {
   let scrollPosition = window.scrollY;
   let scrollMax = height.scrollHeight - screen.height;
-  topBtn.style.opacity = scrollPosition/scrollMax;
+  topBtn.style.opacity = scrollPosition / scrollMax;
 });
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   createButton();
 });
 
-function createButton(){
-  const hover_content = document.querySelectorAll(".hover_content");
-  const hover_info = document.querySelector(".hover_info");
+function createButton() {
+  const hover_content = document.querySelectorAll('.hover_content');
+  const hover_info = document.querySelector('.hover_info');
 
-  hover_content.forEach((item) =>{
-    if(item.innerHTML.length >= 40){
-      const more_button = document.createElement("button")
-      more_button.className="btn";
-      more_button.id="open";
-      more_button.textContent = "더보기";
-      more_button.addEventListener("click", (event)=> seeMore(event));
+  hover_content.forEach((item) => {
+    if (item.innerHTML.length > 75) {
+      const more_button = document.createElement('button');
+      more_button.className = 'btn';
+      more_button.id = 'open';
+      more_button.textContent = '더보기';
+      more_button.addEventListener('click', (event) => seeMore(event));
 
       item.parentNode.appendChild(more_button);
     }
   });
-  
 }
 
-function seeMore(e){
-  e.target.parentNode.querySelector(".hover_content").style.display = "flex";
-  e.target.style.display = "none";
+function seeMore(e) {
+  e.target.parentNode.querySelector('.hover_content').style.display = 'flex';
+  e.target.style.display = 'none';
 }
 
-function leftScroll(e){
-  const preview_container = document.getElementById("preview_container");
-  preview_container.scrollLeft=preview_container.offsetLeft;
- 
+function leftScroll(e) {
+  const preview_container = document.getElementById('preview_container');
+  preview_container.scrollLeft = preview_container.offsetLeft;
 }
 
-function rightScroll(e){
-  const preview_container = document.getElementById("preview_container");
-  preview_container.scrollLeft=preview_container.scrollWidth;
+function rightScroll(e) {
+  const preview_container = document.getElementById('preview_container');
+  preview_container.scrollLeft = preview_container.scrollWidth;
 }
