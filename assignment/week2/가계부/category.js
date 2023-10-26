28803112e4768ef54713ee720e2590266b8ac65c
Soyoung Park <soyoung008@naver.com>
2023-10-26 20:45:59 +0900

    feat : 이벤트 호출러가 두 번 호출되는 문제 해결

--- assignment/week2/가계부/category.js
+++ assignment/week2/가계부/category.js
@@ -22,19 +22,16 @@ const CATEGORY_LIST = [
 ];
 
 document.addEventListener("DOMContentLoaded", () => {
-  if(localStorage.length==0){
+  if (localStorage.length == 0) {
     displayCategory(CATEGORY_LIST);
-    localStorage.setItem("categoryList",JSON.stringify(CATEGORY_LIST));
+    localStorage.setItem("categoryList", JSON.stringify(CATEGORY_LIST));
   }
-  else{
+  else {
     displayCategory(JSON.parse(localStorage.getItem("categoryList")));
   }
- 
+
 });
 
-function once(){
-
-}
 function displayCategory(item) {
   item.map((item) => createCategory(item));
 }
@@ -50,7 +47,7 @@ function createCategory(item) {
   if (item.type == "spend") {
     spend_container.appendChild(li);
   }
-  else {
+  else if(item.type == "income") {
     income_container.appendChild(li);
   }
 }
@@ -59,26 +56,31 @@ function addCategory(e) {
   const income_container = document.querySelector(".income_category_list");
   const spend_container = document.querySelector(".spend_category_list");
   const category_list = JSON.parse(localStorage.getItem("categoryList"));
-  let category_type;
-  if (window.event.keyCode == 13) {
+
+  if (window.event.keyCode == 13 && e.value !="") {
     if (e.id == "income_category_input") {
-      category_type = "income";
+      const new_category = {
+        id: category_list.length + 1,
+        type: "income",
+        name: e.value
+      }
+      category_list.push(new_category);
     }
     else {
-      category_type = "spend";
+      const new_category = {
+        id: category_list.length + 1,
+        type: "spend",
+        name: e.value
+      }
+      category_list.push(new_category);
+      console.log(new_category);
     }
-    const new_category = {
-      id: CATEGORY_LIST.length + 1,
-      type: category_type,
-      name: e.value
-    }
-    console.log(new_category);
-    category_list.push(new_category);
-
-    localStorage.setItem("categoryList",JSON.stringify(category_list));
+    localStorage.setItem("categoryList", JSON.stringify(category_list));
 
     income_container.innerHTML = "";
     spend_container.innerHTML = "";
     displayCategory(JSON.parse(localStorage.getItem("categoryList")));
+    e.value = "";
+    return false;  //이벤트 기본 동작 방지
   }
 }
