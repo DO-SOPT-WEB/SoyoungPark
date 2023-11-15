2de48ef5476cea35ab30857142dd2b7637f85723
Soyoung Park <soyoung008@naver.com>
2023-11-16 01:21:56 +0900

    3주차 과제 pull

---
+++ assignment/week3/lunchMenu/src/App.jsx
@@ -0,0 +1,25 @@
+import { ThemeProvider } from 'styled-components';
+import styled from 'styled-components';
+import GlobalStyle from './style/GlobalStyle';
+import theme from './style/theme';
+import Header from './components/Header';
+import OnBoardingPage from './pages/OnBoardingPage';
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Styled>
+        <OnBoardingPage />
+      </Styled>
+    </ThemeProvider>
+  );
+}
+const Styled = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 2rem;
+  height: 100vh;
+`;
+
+export default App;
