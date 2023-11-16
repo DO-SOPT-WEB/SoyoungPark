a12e89f1beb0264765c53f40b14c8d0de584c0c3
Soyoung Park <soyoung008@naver.com>
2023-11-16 19:50:23 +0900

    feat : 아이디 중복 체크 버튼 구현

--- assignment/week4/login-project/src/pages/SignUp.jsx
+++ assignment/week4/login-project/src/pages/SignUp.jsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
+import axios from 'axios';
 const SignUp = () => {
   const [userData, setUserData] = useState({
     id: '',
@@ -8,19 +9,36 @@ const SignUp = () => {
     confirmPW: '',
     nickname: '',
   });
+  const [check, setCheck] = useState({
+    checkID: '체크전',
+  });
+
+  const API = `http://3.39.54.196/api/v1/members/check?username=${userData.id}`;
 
   const handleUserData = (e) => {
     const { name, value } = e.target;
     setUserData({ ...userData, [name]: value });
+    setCheck({ ...check, checkID: '체크전' });
   };
-  console.log(userData.id);
+  const isExist = () => {
+    axios
+      .get(API)
+      .then((response) => {
+        response.data.isExist ? setCheck({ ...check, checkID: false }) : setCheck({ ...check, checkID: true });
+      })
+      .catch((error) => {
+        console.error('Error checking username:', error);
+      });
+  };
   return (
     <>
       <Header>Sign up</Header>
       <InputWrapper>
         <IdContainer>
           <Input placeholder="아이디를 입력해주세요." onChange={handleUserData} name="id" />
-          <IdCheckBtn>중복체크</IdCheckBtn>
+          <IdCheckBtn onClick={isExist} $checkid={check.checkID}>
+            중복체크
+          </IdCheckBtn>
         </IdContainer>
         <Input placeholder="비밀번호를 입력해주세요." onChange={handleUserData} name="password" />
         <Input placeholder="비밀번호를 다시 한 번 입력해주세요." onChange={handleUserData} name="confirmPW" />
@@ -55,10 +73,11 @@ const Input = styled.input`
 `;
 const IdCheckBtn = styled.button`
   width: 8vw;
-  background-color: ${({ theme }) => theme.colors.black};
-  color: ${({ theme }) => theme.colors.white};
   border: none;
   border-radius: 0.3rem;
+  background-color: ${({ theme, $checkid }) =>
+    $checkid ? ($checkid === '체크전' ? theme.colors.black : theme.colors.green) : theme.colors.red};
+  color: ${({ theme }) => theme.colors.white};
 `;
 const Button = styled.button`
   width: 30vw;
--- assignment/week4/login-project/src/style/theme.js
+++ assignment/week4/login-project/src/style/theme.js
@@ -2,6 +2,8 @@ const colors = {
   black: '#000000',
   white: '#FFFFFF',
   gray: '#d8d8d8',
+  red: '#ff1d00',
+  green: '#007801',
 };
 
 const theme = {
